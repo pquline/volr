915ccebdaa60c6219cc51e20cf80c98c41647cf3
pquline <pfischof@gmail.com>
2025-01-05 22:59:29 +0000

    fix: update footer styles for improved layout and readability

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,19 +6,19 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="text-xs sm:text-sm text-foreground text-center bg-background py-6 pb-4 border-t border-muted space-y-2">
+    <footer className="text-xs sm:text-sm text-foreground text-center bg-background py-6 border-t border-muted space-y-2">
       <div className="justify-between items-center">
-        <div className="space-x-4 text-foreground/95">
+        <div className="space-x-4">
           <Link href="/about" className="hover:underline">About</Link>
           <Link href="/privacy" className="hover:underline">Privacy Policy</Link>
           <Link href="/terms" className="hover:underline">Terms of Service</Link>
         </div>
       </div>
-      <div className="flex justify-center items-center text-md space-x-2">
+      <div className="flex justify-center items-center text-md space-x-2 font-bold">
         <div>Made with</div>
         <Heart className="w-5 h-5 text-destructive animate-pulse" />
         <div>on</div>
-        <Link className="text-tertiary" href="https://github.com/pquline/Volr" target="_blank"><strong>GitHub</strong></Link>
+        <Link className="text-tertiary hover:underline" href="https://github.com/pquline/Volr" target="_blank">GitHub</Link>
       </div>
     </footer>
   );
